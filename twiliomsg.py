920a916f59031bec9cc60d462cfd83b602ae99d3
Farhat Nader <farhatmnader@gmail.com>
2016-04-02 16:45:13 -0700

    added flask framework; http requests

--- twiliomsg.py
+++ twiliomsg.py
@@ -1,16 +1,32 @@
 from twilio.rest import TwilioRestClient
-from random import randint 
+import twilio.twiml
+from random import randint
+from flask import Flask, request
 # Find these values at https://twilio.com/user/account
 
+app = Flask(__name__)
+
 def jokecreator ():
-	myjokedictionary = ["whats hairy and red all over", "the thundercats will live again", "boogers are funny", "farts make people laugh but they stink", 
-						"if God was a bird, he'd be a bird"]
-	return myjokedictionary[randint(0,4)]
+	myjokedictionary = ["whats hairy and red all over", 
+						"the thundercats will live again", 
+						"boogers are funny", 
+						"farts make people laugh but they stink"]
+	return myjokedictionary[randint(0,3)]
 
 
 account_sid = "AC5e15f23d349b00ae2a044fdec01bed42"
 auth_token  = "fb6567ba8daf7960c8fdbe0818b3ec5a"
 client = TwilioRestClient(account_sid, auth_token)
- 
-message = client.messages.create(to="+17186400449", from_="+13478366734",
+
+
+@app.route("/", methods=['GET', 'POST'])
+def main():
+	# if request.method == 'GET': 
+		message = client.messages.create(to="+17186400449", from_="+13478366734",
                                      body= jokecreator() )
+
+		return message;
+	
+
+if __name__ == "__main__":
+    app.run(debug=True)
