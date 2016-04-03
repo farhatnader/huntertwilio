f7466a886795e7e81ccfbebb5c8d4a6722a4b011
hh <df>
2016-04-03 00:15:08 -0400

    newest commit increased 10000 for iteration for gif search in Json

--- twiliomsg.py
+++ twiliomsg.py
@@ -9,33 +9,31 @@ import json
 
 app = Flask(__name__)
 
-
-def jokecreator ():
-    myjokedictionary = ["whats hairy and red all over", 
-    					"http://www.vitaminbeyonce.com/", 
-    					"http://www.randomgoat.com/", 
-    					"farts make people laugh but they stink"]
-    return myjokedictionary[randint(0,3)]
-
  
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
     if request.method == "GET":
         user_request = request.values.get('Body', None)
 
+        print(user_request)
+
         data = json.loads(urllib.urlopen("http://api.giphy.com/v1/gifs/search?q=" 
         		+ user_request 
-        		+ "&api_key=dc6zaTOxFJmzC&limit=5").read())
+        		+ "&api_key=dc6zaTOxFJmzC&limit=10000").read())
 
+        list_of_urls = []
 
-        # message = jokecreator();
-        message = random.choice(d.keys(data[0].images.fixed_height_small.url))
+        for thing in data['data']:
+        	print(thing['images']['fixed_height']['url'])
+        	list_of_urls.append(thing['images']['fixed_height']['url'])
+
+        message = random.choice(list_of_urls)
 
         resp = twilio.twiml.Response()
         resp.message(message)
 
         return str(resp)
-
+        
 
 if __name__ == "__main__":
     app.run(debug = True)
