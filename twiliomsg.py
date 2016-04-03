806c47deaed17a35f83a2c5d766a8a2d66ee3279
Farhat Nader <farhatmnader@gmail.com>
2016-04-02 19:31:17 -0700

    added json data from giphy  api; added method to parse gif urls

--- twiliomsg.py
+++ twiliomsg.py
@@ -1,38 +1,41 @@
 from flask import Flask, request, redirect
 from twilio.rest import TwilioRestClient
-from random import randint 
-
+from random import randint
+import random 
 import twilio.twiml
- 
-app = Flask(__name__)
- 
-def jokecreator ():
-    myjokedictionary = ["whats hairy and red all over", "http://www.vitaminbeyonce.com/", "http://www.randomgoat.com/", "farts make people laugh but they stink", 
-                        "if God was a bird, he'd be a bird"]
-    return myjokedictionary[randint(0,4)]
+import urllib
+import json
 
-# Try adding your own number to this list!
-callers = {
-    "+17184165926": "Curious George",
-    "+14158675310": "Boots",
-    "+14158675311": "Virgil",
-    "+17184165926": "Chris"
-}
+
+app = Flask(__name__)
+
+
+def jokecreator ():
+    myjokedictionary = ["whats hairy and red all over", 
+    					"http://www.vitaminbeyonce.com/", 
+    					"http://www.randomgoat.com/", 
+    					"farts make people laugh but they stink"]
+    return myjokedictionary[randint(0,3)]
+
  
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
-    """Respond and greet the caller by name."""
-    if request.method=="GET":
-        from_number = request.values.get('From', None)
-        if from_number in callers:
-            message = jokecreator();
-        else:
-            message = "Monkey, thanks for the message!"
+    if request.method == "GET":
+        user_request = request.values.get('Body', None)
+
+        data = json.loads(urllib.urlopen("http://api.giphy.com/v1/gifs/search?q=" 
+        		+ user_request 
+        		+ "&api_key=dc6zaTOxFJmzC&limit=5").read())
+
+
+        # message = jokecreator();
+        message = random.choice(d.keys(data[0].images.fixed_height_small.url))
 
         resp = twilio.twiml.Response()
         resp.message(message)
 
         return str(resp)
- 
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug = True)
