fd45440393629d42802efce6b93f5c97e259857c
Farhat Nader <farhatmnader@gmail.com>
2016-04-02 20:26:41 -0700

    fixed json parsing; enabled sending of an appropriate url back to user

--- twiliomsg.py
+++ twiliomsg.py
@@ -21,20 +21,20 @@ def jokecreator ():
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
     if request.method == "GET":
-        user_request = request.values.get('Body', None)
+       user_request = request.values.get('Body', None)
 
-        data = json.loads(urllib.urlopen("http://api.giphy.com/v1/gifs/search?q=" 
-        		+ user_request 
-        		+ "&api_key=dc6zaTOxFJmzC&limit=5").read())
+       data = json.loads(urllib.urlopen("http://api.giphy.com/v1/gifs/search?q="
+               + user_request
+               + "&api_key=dc6zaTOxFJmzC&limit=5").read())
+       list_of_urls = []
+       for thing in data['data']:
+           print(thing['images']['fixed_height']['url'])
+           list_of_urls.append(thing['images']['fixed_height']['url'])
 
-
-        # message = jokecreator();
-        message = random.choice(d.keys(data[0].images.fixed_height_small.url))
-
-        resp = twilio.twiml.Response()
-        resp.message(message)
-
-        return str(resp)
+       message = random.choice(list_of_urls)
+       resp = twilio.twiml.Response()
+       resp.message(message)
+       return str(resp)
 
 
 if __name__ == "__main__":
